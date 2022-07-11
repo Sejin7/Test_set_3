a8bdbc770749eb68530e4f2be2035b1639e5523e
Sejin7 <universe713@gmail.com>
2022-07-11 21:08:01 +0900

    Baekjoon problem codes

---
+++ src/baekjoon_7_5.java
@@ -0,0 +1,18 @@
+// 각 테스트 데이터마다 정확히 한 행을 출력하는데, 내용은 N 번째 손님에게 배정되어야 하는 방 번호를 출력한다.
+public class baekjoon_7_5 {
+        int[] data = {6, 6, 6};
+        int num = 1;
+
+        void solution_7_5() {
+            for(int i=0; i<num; i++) {
+                int floor = data[2]%data[0];
+                int roomNum = data[2]/data[0]+1;
+
+                if(floor == 0) {
+                    floor = data[0];
+                    roomNum = roomNum - 1;
+                }
+                System.out.printf("%d%02d", floor, roomNum);
+            }
+        }
+}
---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main{
+    public static void main(String[] args) {// 호텔방 호수 출력하기
+        baekjoon_7_5 code_7_5 = new baekjoon_7_5();
+        code_7_5.solution_7_5();
+    }
+}
